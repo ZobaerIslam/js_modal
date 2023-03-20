f4c536f38b281f07281083b478d8a667b3dbe8ec
zobaer <zobaerislam62@gmail.com>
2023-03-20 11:42:32 +0600

    implement open and close modal with click

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClassModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for(let i=0; i<btnsOpenModal.length; i++) {
+    // Open modal in click button
+    btnsOpenModal[i].addEventListener(
+        'click',
+        function() {
+            modal.classList.remove('hidden');
+            overlay.classList.remove('hidden');
+        }
+    );
+
+    // Close modal click with close icon
+    btnClassModal.addEventListener(
+        'click', 
+        function() {
+            modal.classList.add('hidden');
+            overlay.classList.add('hidden');
+        }
+    );
+    
+    // Close modal click with overlay
+    overlay.addEventListener(
+        'click',
+        function() {    
+            modal.classList.add('hidden');
+            overlay.classList.add('hidden');
+        }
+    );
+}
